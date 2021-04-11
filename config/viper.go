e3a463b2dba925402b3da0dcb3c40efbc9c38239
Eldius <eldiosantos@gmail.com>
2021-04-11 13:41:49 -0300

    Trying to change some things here...

--- config/viper.go
+++ config/viper.go
@@ -12,24 +12,13 @@ import (
 )
 
 func SetDefaults() {
-	viper.SetDefault("auth.database.url", "test.db")
-	viper.SetDefault("auth.database.engine", "sqlite3")
+	//viper.SetDefault("auth.database.url", "test.db")
+	//viper.SetDefault("auth.database.engine", "sqlite3")
 	viper.SetDefault("auth.user.pattern", "^[a-zA-Z0-9\\._-]*$")
 	viper.SetDefault("auth.pass.pattern", "^[a-zA-Z0-9\\._-]*$")
 	viper.SetDefault("auth.jwt.secret", uuid.New().String())
 	viper.SetDefault("auth.user.default.active", true)
 	viper.SetDefault("auth.jwt.ttl", "0s")
-}
-
-func BindEnvVars() {
-	bindEnv("auth.database.url", "AUTH_DATABASE_URL")
-	bindEnv("auth.database.engine", "AUTH_DATABASE_ENGINE")
-	bindEnv("auth.database.log", "AUTH_DATABASE_LOG")
-	bindEnv("auth.user.pattern", "AUTH_USER_PATTERN")
-	bindEnv("auth.pass.pattern", "AUTH_PASS_PATTERN")
-	bindEnv("auth.jwt.secret", "AUTH_JWT_SECRET")
-	bindEnv("auth.user.default.active", "AUTH_USER_DEFAULT_ACTIVE")
-	bindEnv("auth.jwt.ttl", "AUTH_JWT_TTL")
 }
 
 func SetupViper(cfgFile string) {
@@ -50,7 +39,6 @@ func SetupViper(cfgFile string) {
 		viper.SetConfigType("yml")
 	}
 	SetDefaults()
-	BindEnvVars()
 	viper.AutomaticEnv() // read in environment variables that match
 
 	// If a config file is found, read it in.
